f6f6f3ad3e7b3dd579d1215d4af6ec5704357aed
김연진 <111187984+yeonjin719@users.noreply.github.com>
2023-09-15 13:43:50 +0900

    Update hw1.c

--- hw1.c
+++ hw1.c
@@ -3,7 +3,7 @@ int num1;
 int num2;
 
 int main(){
-    printf("Input two integers:");
+    printf("Input two integers: ");
     scanf("%d %d", &num1, &num2);
     printf("%d&%d = %d\n", num1, num2, num1&num2);
     printf("%d|%d = %d\n", num1, num2, num1|num2);
